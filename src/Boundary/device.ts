e408b7ebc38a18b8940a63be0c8ded02ae8e0ead
TAKAHASHI Shuuji <shuuji3@gmail.com>
2020-01-15 22:36:14 +0900

    単純に1番目のタスクを通知する機能を追加

--- src/Boundary/device.ts
+++ src/Boundary/device.ts
@@ -1,10 +1,10 @@
 import { GetWholeDeviceWithUser } from "./device.d";
 import { QueryArgs } from "./psql_util.d";
 
-export const get_whole_device_with_user: GetWholeDeviceWithUser = async client => {
+export const getWholeDeviceWithUser: GetWholeDeviceWithUser = async client => {
   const queryargs: QueryArgs = {
     query:
-      "SELECT name, endpoint, auth, p256dh FROM user_device LEFT OUTER JOIN users ON user_device.user_id= users.id",
+      "SELECT name, endpoint, auth, p256dh, user_id FROM user_device LEFT OUTER JOIN users ON user_device.user_id = users.id",
     params: []
   };
   let res = client.eval_query(queryargs);
--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,16 @@ import cron = require("node-cron");
 import { psql_client } from "./connection_info";
 import { PSQLClient } from "./Boundary/psql_util";
 import { QueryArgs } from "./Boundary/psql_util.d";
-import { get_whole_device_with_user } from "./Boundary/device";
 import { sendNotification } from "web-push";
 import { CronSendNotifications } from "./index.d";
 import { WebPush } from "./WebPush/webpush_util";
 import { VapidDetail, Subscription, Message } from "./WebPush/webpush_util.d";
 import { vapid_detail } from "./vapid_keys";
+import { getWholeDeviceWithUser } from "./Boundary/device";
+import { getTasksByUserID } from "./Boundary/task";
 
 const psqlclient = new PSQLClient(psql_client);
 const webpush = new WebPush(vapid_detail);
-const appname = "task-cabinet";
 psqlclient.connect_client();
 
 interface TestMessage {
@@ -20,38 +20,49 @@ interface TestMessage {
   actions: any;
 }
 const cronSendNotifications: CronSendNotifications = async psqlclient => {
-  console.log("[info] scheduled notification");
-  let res = await get_whole_device_with_user(psqlclient);
-  res.forEach(r => {
+  let deviceWithUsers = await getWholeDeviceWithUser(psqlclient);
+  for (const deviceWithUser of deviceWithUsers) {
+    // Get tasks by the given user
+    const userID = deviceWithUser.user_id;
+    const tasks = await getTasksByUserID(psqlclient, userID);
+    const notifiedTask = tasks[0]; // TODO: アルゴリズムによる選択
+
+    // Create notification
     const subs: Subscription = {
-      endpoint: r.endpoint,
+      endpoint: deviceWithUser.endpoint,
       keys: {
-        auth: r.auth,
-        p256dh: r.p256dh
+        auth: deviceWithUser.auth,
+        p256dh: deviceWithUser.p256dh
       }
     };
     let message = {
       notification: {
-        title: `Push Notification from ${appname}`, // TODO: write
-        body: `Are you ${r.name} ?`, // TODO: fill task name
-        icon: "assets/main-page-logo-small-hat.png",
+        title: `${deviceWithUser.name} さんのタスクのお知らせ`,
+        body: `${notifiedTask.name} (見積り: ${notifiedTask.estimate})`,
+        icon: "assets/icons/icon-72x72.png",
         vibrate: [100, 50, 100],
         data: { message: `Hello! You have a task.` },
-        actions: [{ action: "explore", title: "Go to the site" }]
+        actions: [{ action: "explore", title: "アプリを開く" }]
       }
     };
     webpush
       .send(subs, JSON.stringify(message))
       .then(_ =>
-        console.log(`correct push for ${r.name}-${r.endpoint}-${message}`)
+        console.log(
+          `[info] pushed to '${deviceWithUser.name}' via '${
+            deviceWithUser.endpoint
+          }' message: ${JSON.stringify(message)}`
+        )
       )
-      .catch(err => console.log("[ERROR]"));
-  });
+      .catch(err => console.log(`[error] ${err}`));
+    console.log("[info] message:");
+    console.log(message);
+  }
 };
 
 // ref: https://chuckwebtips.hatenablog.com/entry/2017/04/23/173359
 // ref: https://qiita.com/n0bisuke/items/66abf6ca1c12f495aa04
 cron.schedule("* * * * *", async () => {
-  console.log("hello from typescript via cron");
+  console.log("[info] run cron");
   await cronSendNotifications(psqlclient);
 });
